28fa7d2dd1543021b0465a827353840c7d7356b3
ysosrss <najib72645@gmail.com>
2022-10-27 18:36:44 +0800

    Add more style

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-for (i=0;i<256;i++) {
-    var div=document.createElement('div');
-    div.classList.add('griditems');
-    div.innerText = "h";
 
-    document.getElementById('container').appendChild(div);
+function generateGrid (number) {
+    for (i=0;i<(number*number);i++) {
+        var div=document.createElement('div');
+        div.classList.add('griditems');
+        document.getElementById('container').appendChild(div);
+    }
 }
+
+let cols = 16;
+generateGrid (cols);
+
+const container = document.getElementById('container');
+container.style.setProperty('--grid-cols', cols);
+
+
 
 
 const gridItems = document.querySelectorAll("div.griditems");
-console.log(gridItems);
 
 for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener('mouseenter', () => {
         gridItems[i].classList.add('color');
     });
-/*
-    gridItems[i].addEventListener('mouseleave', () => {
-        gridItems[i].classList.toggle('color');
-    });
-    */
 }
- 
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', resetGrid);
+
+function resetGrid() {
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].classList.remove('color');
+    }
+}
+
 
