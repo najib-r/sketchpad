371a6251dda660009cb892c41a8379bc5df8f0e1
ysosrss <najib72645@gmail.com>
2022-10-28 09:29:24 +0800

    Fix grid size change

--- script.js
+++ script.js
@@ -1,46 +1,75 @@
 const container = document.getElementById('container');
 
+
+let cols = 16;
+generateGrid (cols);
+container.style.setProperty('--grid-cols', cols);
+container.style.setProperty('--grid-rows', cols);
+
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', reloadGrid); 
+
+const gridsize = document.getElementById('gridsize');
+gridsize.addEventListener('click', changeGrid);
+
+const toggle = document.getElementById('toggle');
+toggle.addEventListener('click', toggleGrid);
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
 function generateGrid (number) {
     for (i=0;i<(number*number);i++) {
         var div=document.createElement('div');
         div.classList.add('griditems');
         container.appendChild(div);
     }
-}
-
-function resetGrid() {
+    const gridItems = document.querySelectorAll("div.griditems");
     for (let i = 0; i < gridItems.length; i++) {
-        gridItems[i].classList.remove('color');
+        gridItems[i].addEventListener('mouseover', () => {
+            gridItems[i].classList.add('color');
+        });
     }
+    container.style.setProperty('--grid-cols', cols);
+    container.style.setProperty('--grid-rows', cols);
 }
 
 
-let cols = 16;
-generateGrid (cols);
-container.style.setProperty('--grid-cols', cols);
-
-const gridItems = document.querySelectorAll("div.griditems");
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].addEventListener('mouseenter', () => {
-        gridItems[i].classList.add('color');
-    });
+function changeGrid () {
+    changeSize();
+    reloadGrid();
 }
-
-const reset = document.getElementById('reset');
-reset.addEventListener('click', resetGrid); 
-
-const gridsize = document.getElementById('gridsize');
-gridsize.addEventListener('click', changeSize);
-
-
 
 
 function changeSize () {
     do {
-        let cols = prompt("Please enter a grid size of not more than 100.")
-    } while (cols > 100);
-    container.replaceChildren();
+        cols = prompt("Please enter a grid size of not more than 100.");
+    } while (cols > 100 || cols === "");
+    
+    console.log(cols);
+}
+
+function reloadGrid() {
+    container.innerHTML='';
+    console.log(cols);
     generateGrid(cols);
-    container.style.setProperty('--grid-cols', cols);
+    const gridItems = document.querySelectorAll("div.griditems");
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].addEventListener('mouseover', () => {
+            gridItems[i].classList.add('color');
+        });
+    }
+
+}
+
+function toggleGrid() {
+    const gridItems = document.querySelectorAll("div.griditems");
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].classList.toggle('showgrid');
+    }
 }
 
